0f85e493e979b3891c360e311e104903c3969021
uo283928 <113936619+uo283928@users.noreply.github.com>
2022-12-07 03:42:27 +0100

    Add files via upload

--- Ejercicio11.js
+++ Ejercicio11.js
@@ -4,7 +4,24 @@ class GeoLocalizacion {
             this.latitude = position.coords.latitude;
             this.longitude= position.coords.longitude;
             document.write("<p>La posición del usuario es: " + this.latitude + ", " + this.longitude + "</p>");
-          });
+        }, (error=>{
+            switch(error.code) {
+                case error.PERMISSION_DENIED:
+                    this.mensaje = "El usuario no permite la petición de geolocalización"
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    this.mensaje = "Información de geolocalización no disponible"
+                    break;
+                case error.TIMEOUT:
+                    this.mensaje = "La petición de geolocalización ha caducado"
+                    break;
+                case error.UNKNOWN_ERROR:
+                    this.mensaje = "Se ha producido un error desconocido"
+                    break;
+                }
+                
+                document.write("<p>" + this.mensaje + "</p>");
+          }));
         
     }
    
