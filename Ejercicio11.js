c69b7785864eee95e7cb13e23e7c60087ee82472
uo283928 <113936619+uo283928@users.noreply.github.com>
2022-12-07 03:46:56 +0100

    Add files via upload

--- Ejercicio11.js
+++ Ejercicio11.js
@@ -1,39 +1,53 @@
 class GeoLocalizacion {
-    constructor(){
-        navigator.geolocation.getCurrentPosition(position => {
-            this.latitude = position.coords.latitude;
-            this.longitude= position.coords.longitude;
-            document.write("<p>La posición del usuario es: " + this.latitude + ", " + this.longitude + "</p>");
-        }, (error=>{
-            switch(error.code) {
-                case error.PERMISSION_DENIED:
-                    this.mensaje = "El usuario no permite la petición de geolocalización"
-                    break;
-                case error.POSITION_UNAVAILABLE:
-                    this.mensaje = "Información de geolocalización no disponible"
-                    break;
-                case error.TIMEOUT:
-                    this.mensaje = "La petición de geolocalización ha caducado"
-                    break;
-                case error.UNKNOWN_ERROR:
-                    this.mensaje = "Se ha producido un error desconocido"
-                    break;
-                }
-                
-                document.write("<p>" + this.mensaje + "</p>");
-          }));
+    
+    constructor (){
+        navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this), this.verErrores.bind(this));
         
     }
+    getPosicion(posicion){
+        this.mensaje = "Se ha realizado correctamente la peticiÃ³n de geolocalizaciÃ³n";
+        this.longitud         = posicion.coords.longitude; 
+        this.latitud          = posicion.coords.latitude;  
+        this.precision        = posicion.coords.accuracy;
+        this.altitud          = posicion.coords.altitude;
+        this.precisionAltitud = posicion.coords.altitudeAccuracy;
+        this.rumbo            = posicion.coords.heading;
+        this.velocidad        = posicion.coords.speed;       
+    }
+    verErrores(error){
+        switch(error.code) {
+        case error.PERMISSION_DENIED:
+            this.mensaje = "El usuario no permite la peticiÃ³n de geolocalizaciÃ³n"
+            break;
+        case error.POSITION_UNAVAILABLE:
+            this.mensaje = "InformaciÃ³n de geolocalizaciÃ³n no disponible"
+            break;
+        case error.TIMEOUT:
+            this.mensaje = "La peticiÃ³n de geolocalizaciÃ³n ha caducado"
+            break;
+        case error.UNKNOWN_ERROR:
+            this.mensaje = "Se ha producido un error desconocido"
+            break;
+        }
+    
+    }
+    map;
+    initialize() {
+        var mapOptions = {
+        zoom: 8,
+        center: new google.maps.LatLng( 43.3661,  -5.8425 )
+      };
+      this.map = new google.maps.Map(document.getElementsByName('mapa-canvas')[0], mapOptions);
+      return this.map;
+      //google.maps.event.addDomListener(window, 'load', initialize);
+      
+    }
+
+    seeMap() {
+        console.log(google.maps.event.addDomListener(window, 'load', this.initialize()));
+    }
+   
    
 }
 
-class GeoManager{
-    constructor(){}
-
-    pedirPosicion() {
-        var geo = new GeoLocalizacion();
-    }
-
-}
-
-var gm = new GeoManager;
+var miMapa = new GeoLocalizacion;
